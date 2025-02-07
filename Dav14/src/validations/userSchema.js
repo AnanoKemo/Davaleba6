23fe24b0afc4695892bd4c5f9ca6ddce17929fc2
AnanoKemo <origami.georgia1@gmail.com>
2025-02-08 01:07:39 +0400

    Start fresh with only finaluri

--- Dav14/src/validations/userSchema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup'
-
-export const userSchema = Yup.object().shape({
-    firstName: Yup.string().required('FirstName is required'),
-    lastName: Yup.string().required('LastName is required'),
-    email: Yup.string().email('invalid email').required('email is required'),
-    mobile: Yup.string().required('mobile is required').matches(/^\d+$/, {message: 'wrong format'}).length(9, 'wrong mobile')
-})
--- Lec15/src/validations/userSchema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup'
-
-export const userSchema = Yup.object().shape({
-    fullName: Yup.string().required('fullname is required'),
-    email: Yup.string().email('invalid email').required('email is required'),
-    age: Yup.number().required('age is required').min(10, 'min age is 10').max(80, 'max age is 80'),
-    mobile: Yup.string().required('mobile is required').matches(/^\d+$/, {message: 'wrong format'}).length(9, 'wrong mobile')
-})
